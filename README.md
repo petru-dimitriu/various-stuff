4ae20e60553570a21a26d4c2cea3df4462f93bee
Petru Dimitru <dimtriupetru@gmail.com>
2016-05-27 17:15:35 +0300

    Added sudoku

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Various stuff
-Various small interesting things I've written, mostly as part of faculty homework, or otherwise unworthy of having a separate repository.
+Various small and potentially interesting things I've written, mostly as part of faculty homework, or otherwise unworthy of having a separate repository.
 
-* **text-draw-funcs**: Elementary functions for drawing in (console) under Windows; uses Bresenham's line algorithm
+### Hardware description
 * **counting-sort-asm**: Counting sort algorithm implemented in x86 assembly
+* **digital-system-for-recursive-computation**: Verilog implementation of a digital syste*
+* **single-cycled-mips-processor**: Verilog implementation of a MIPS processor supporting add, sub, sw, lw, beq instructions, with 1 cycle/instruction. Also contains a working code and data memory and a testbench for testing.m (data path + control path) capable of computing elements of the recursive sequence x<sub>n</sub> = 14*x<sub>n-1</sub>+n, with x<sub>0</sub>=0. Consists of several simple basic modules for sumation, comparison, etc, each with a minimal testbench module.
+
+### Algorithms
 * **karatsuba**: Karatsuba's multiplication algorithm implemented for any numeration base
-* **digital-system-for-recursive-computation**: Verilog implementation of a digital system (data path + control path) capable of computing elements of the recursive sequence x<sub>n</sub> = 14*x<sub>n-1</sub>+n, with x<sub>0</sub>=0. Consists of several simple basic modules for sumation, comparison, etc, each with a minimal testbench module.
-* **single-cycled-mips-processor**: Verilog implementation of a MIPS processor supporting add, sub, sw, lw, beq instructions, with 1 cycle/instruction. Also contains a working code and data memory and a testbench for testing.
 * **optimal-weighted-bin-tree**: C++ algorithm to construct a binary tree so that its weighted external path length is minimal
 * **huffman-encoding**: C++ program which reads a word and constructs a Huffman encoding, then encodes the word and prints it. Uses a heap of binary trees.
+
+### Miscellaneous
+* **text-draw-funcs**: Elementary functions for drawing in (console) under Windows; uses Bresenham's line algorithm
+* **sudoku**: Yet another take at implementing a sudoku solver in C++. Features comments with explanations in Romanian.
---
+++ sudoku/sudoku.cpp
@@ -0,0 +1,127 @@
+/*
+ * sudoku.cpp
+ *
+ */
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+ifstream in("sudoku.in");
+ofstream out("sudoku.out");
+
+// Liniile, coloanele si subpatratele tabloului se numeroteaza cu 0 ... 8
+// subpatratele sunt numerotate asa:
+/*  - - - - - - -
+ *  | 0 | 1 | 2 |
+ *  - - - - - - -
+ *  | 3 | 4 | 5 |
+ *  - - - - - - -
+ *  | 6 | 7 | 8 |
+ *  - - - - - - -
+ */
+
+int t[9][9];			   // tabloul propriu-zis
+bool patrat[9][10];		   // patrat[x][y] = 1, daca numarul y se afla in patratul x
+bool linie[9][10];		   // linie[x][y] = 1, daca numarul y se afla pe linia x
+bool coloana[9][10];	   // coloana[x][y] = 1, daca numarul y se afla pe coloana x
+bool gata;				   // = 1 daca s-a gasit o solutie, deci nu are sens sa se mai caute
+
+// functie recursiva de rezolvare
+void rezolva(int x, int y)
+{
+	int i,j,urmx=0,urmy=0;
+	/* caut, pentru inceput, casuta pe care o am de umplut
+	 * dupa ce umplu casuta curenta
+	 */
+	for (i=x, j=y+1;i<9 && !urmx && !urmy;i++)
+	{
+
+		for (;j<9 && !urmx && !urmy;j++)
+		{
+			if (t[i][j] == 0)
+			{
+				urmx = i;
+				urmy = j;
+			}
+		}
+		j = 0;
+	}
+
+	// incerc, pe rand, toate variantele pentru casuta curenta
+	for (i=1;i<=9 && !gata;i++)
+	{
+		if (linie[x][i] == 0  && // daca nu se afla pe linia x
+			coloana[y][i] == 0  // si nu se afla pe coloana y
+			&& patrat[(x/3) * 3 + y/3][i] == 0) // si nu se afla nici in patratul curent
+		{
+			// poate fi adaugat
+			t[x][y] = i; // adaugam in tabel
+			// marcam ca pe linia, coloana,si in patratul curent,
+			// s-a pus deja numarul pe care l-am asezat, deci nu il mai putem pune dupa el
+			linie[x][i] = 1;
+			coloana[y][i] = 1;
+			patrat[(x/3) * 3 + y/3][i] = 1;
+			// si trecem mai departe la urmatoarul patratel de umplut
+			if (urmx != 0 || urmy != 0) // asta daca mai exista un patratel gol
+			{
+				rezolva(urmx,urmy);
+				// daca incercarea de a rezolva sudokuul cu numarul curent pe pozitia asta a esuat
+				if (!gata)
+				{
+					// demarcam tot ce marcasem
+					linie[x][i] = 0;
+					coloana[y][i] = 0;
+					patrat[(x/3) * 3 + y/3][i] = 0;
+					t[x][y] =  0;
+				}
+			}
+			else // daca nu mai exista niciun patratel gol inseamna ca am rezolvat sudokuul
+			{
+				gata = 1; // marchez ca am terminat; se vor incheia pe rand toate functiile
+			}
+		}
+	}
+}
+
+void afiseaza()
+{
+	int i, j;
+	for (i=0;i<9;i++)
+	{
+		for (j=0;j<9;j++)
+			out << t[i][j] << " ";
+		out << ""\n";
+	}
+}
+
+int main()
+{
+	int i, j, n, x, y, v;
+	in >> n;
+	for (i=1;i<=n;i++)
+	{
+		in >> x >> y >> v;
+		x--; y--; // scad 1 pentru ca am numerotat intern lin si col de la 0
+		t[x][y] = v; // introduc nr in tabel
+		patrat[(x/3) * 3 + y/3][v] = 1; // marchez ca in subpatrat am numarul introdus
+		linie[x][v] = 1; // marchez ca pe lin exista numarul tocmai introdus, deci nu trebuie reintrodus
+		coloana[y][v] = 1; // marchez ca pe col exista numarul tocmai introdus
+	}
+	// Cautam primul patratel gol ca sa incepem algoritmul de acolo
+	for (i=0;i<9 && !gata;i++)
+	{
+		for (j=0;j<9 && !gata;j++)
+		{
+			if (t[i][j] == 0)
+			{
+				// functia de rezolvare va seta variabila globala gata la 1,
+				// ceea ce va duce automat la incheierea for-urilor
+				rezolva(i,j);
+				afiseaza();
+			}
+		}
+		j = 0;
+	}
+	return 0;
+}
+
